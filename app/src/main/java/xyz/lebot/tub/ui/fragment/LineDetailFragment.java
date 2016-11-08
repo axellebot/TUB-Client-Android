fe9ed2b0381b76d8a7601811c6dabbe621bcf26b
p1401648 <nico6959.nc@gmail.com>
2016-11-08 17:46:49 +0100

    WIP: add line detail view

--- app/src/main/java/xyz/lebot/tub/ui/fragment/LineDetailFragment.java
+++ app/src/main/java/xyz/lebot/tub/ui/fragment/LineDetailFragment.java
@@ -1,28 +1,55 @@
 package xyz.lebot.tub.ui.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import xyz.lebot.tub.R;
+import xyz.lebot.tub.data.model.LineModel;
+import xyz.lebot.tub.ui.manager.GridAutofitLayoutManager;
+import xyz.lebot.tub.ui.presenter.LineDetailFragmentPresenter;
+import xyz.lebot.tub.ui.presenter.LineFragmentPresenter;
 
-/**
- * Created by axellebot on 08/11/2016.
- */
+public class LineDetailFragment extends android.support.v4.app.Fragment {
 
-public class LineDetailFragment extends Fragment {
+    private LineDetailFragmentPresenter presenter;
+
 
     public LineDetailFragment() {
         // Required empty public constructor
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_line_detail,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_line_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        presenter = new LineDetailFragmentPresenter(this);
+        presenter.initialize();
         return view;
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.pause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter.resume();
+    }
+
+    public void initView(LineModel lineModel) {
+        //lineGridAdapter.swap(lineModels);
+    }
 }
+
